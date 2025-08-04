500b8ea42ed18fb620c25864df76e78fcfc7385d
DESKTOP-PMID8VC\USER <crynise@naver.com>
2025-08-04 15:25:30 +0900

    Mybatis

---
+++ src/main/java/kr/ac/kopo/ctc/kopo21/board/repository/SampleMapper.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.ctc.kopo21.board.repository;
+
+import kr.ac.kopo.ctc.kopo21.board.domain.Sample;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+
+@Mapper
+public interface SampleMapper {
+    List<Sample> findAll();
+    Sample findById(Long id);
+    List<Sample> findAllByTitle(String title, RowBounds rowBounds);
+
+
+}
