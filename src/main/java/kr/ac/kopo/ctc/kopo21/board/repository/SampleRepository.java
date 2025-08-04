7e99ee8f3edac941224250eaee8edf357cbe146d
DESKTOP-PMID8VC\USER <crynise@naver.com>
2025-08-04 13:13:20 +0900

    스프링 3-1강

---
+++ src/main/java/kr/ac/kopo/ctc/kopo21/board/repository/SampleRepository.java
@@ -0,0 +1,10 @@
+package kr.ac.kopo.ctc.kopo21.board.repository;
+
+
+import kr.ac.kopo.ctc.kopo21.board.domain.Sample;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleRepository extends JpaRepository<Sample, Long> {
+
+
+}
---
+++ src/test/java/kr/ac/kopo/ctc/kopo21/board/repository/SampleRepositoryTest.java
@@ -0,0 +1,22 @@
+package kr.ac.kopo.ctc.kopo21.board.repository;
+
+import kr.ac.kopo.ctc.kopo21.board.domain.Sample;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.domain.AbstractAuditable_;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class SampleRepositoryTest {
+    @Autowired
+    SampleRepository sampleRepository;
+    @Test
+    void findAll(){
+        List<Sample> list = sampleRepository.findAll();
+        assertEquals(2, list.size());
+    }
+}
